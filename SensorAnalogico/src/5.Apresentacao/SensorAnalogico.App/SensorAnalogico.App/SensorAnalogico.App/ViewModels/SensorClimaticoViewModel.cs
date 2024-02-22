207e2f52cb1f3af46b8d38582ea0004f291d91da
Alex Pimenta <alex.pimenta@agriculturaverion.com.br>
2024-02-22 09:11:39 -0300

    Implementação do Sensor Climático

---
+++ SensorAnalogico/src/5.Apresentacao/SensorAnalogico.App/SensorAnalogico.App/SensorAnalogico.App/ViewModels/SensorClimaticoViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using ReactiveUI;
+using SensorAnalogico.App.Services;
+using System;
+using System.Windows.Input;
+
+namespace SensorAnalogico.App.ViewModels
+{
+    public class SensorClimaticoViewModel : ViewModelBase
+    {
+
+        private IBrush statusColor = Brushes.Red;
+
+        public IBrush StatusColor
+        {
+            get => statusColor; 
+            set => this.RaiseAndSetIfChanged(ref statusColor, value);
+        }
+
+        private IBrush commandColor = Brushes.Red;
+
+        public IBrush CommandColor
+        {
+            get => commandColor;
+            set => this.RaiseAndSetIfChanged(ref commandColor, value);
+        }
+
+        private string microcontrollerId = "0";
+        public string MicrocontrollerId
+        {
+            get => microcontrollerId;
+            set => this.RaiseAndSetIfChanged(ref microcontrollerId, value);
+        }
+
+        private string componentId = "0";
+        public string ComponentId
+        {
+            get => componentId;
+            set => this.RaiseAndSetIfChanged(ref componentId, value);
+        }
+
+        public AnalogicService Service { get; set; } = new();
+
+        public ICommand CommandGoBack { get; }
+        public ICommand CommandChangeConfiguration { get; }
+        public ICommand CommandChangeCommand { get; }
+        public ICommand CommandGetInput { get; }
+
+        public SensorClimaticoViewModel()
+        {
+            StatusColor = Brushes.Red;
+            CommandColor = Brushes.Green;
+            CommandGoBack = ReactiveCommand.Create((TransitioningContentControl tcc) => { CommandGoBack_Action(tcc); });
+            CommandChangeConfiguration = ReactiveCommand.Create((TransitioningContentControl tcc) => { CommandChangeConfiguration_Action(tcc); });
+            CommandChangeCommand = ReactiveCommand.Create(CommandChangeCommand_Action);
+            CommandGetInput = ReactiveCommand.Create(CommandGetInput_Action);
+        }
+
+        private void CommandGoBack_Action(TransitioningContentControl tcc)
+        {
+            NavigateTo(tcc, ResourcePages.PageName.Component);
+        }
+
+        private void CommandChangeConfiguration_Action(TransitioningContentControl tcc)
+        {
+            NavigateTo(tcc, ResourcePages.PageName.ConfigurationAnalogicInput);
+        }
+
+        private void CommandChangeCommand_Action()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommandGetInput_Action()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
