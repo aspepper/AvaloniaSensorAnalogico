6236326386df72343e557af77d3c1989988c1e4b
Alex Pimenta <alex.pimenta@agriculturaverion.com.br>
2024-03-11 15:30:41 -0300

    Commit do Stage

---
+++ SensorAnalogico/src/5.Apresentacao/SensorAnalogico.App/SensorAnalogico.App/SensorAnalogico.App/ViewModels/MarcadorDePosicaoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorAnalogico.App.ViewModels
+{
+    public class MarcadorDePosicaoViewModel : ViewModelBase
+    {
+
+        public MarcadorDePosicaoViewModel() { }
+
+
+
+    }
+}
---
+++ SensorAnalogico/src/5.Apresentacao/SensorAnalogico.App/SensorAnalogico.App/SensorAnalogico.App/Views/MarcadorDePosicaoView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SensorAnalogico.App.Views
+{
+    public partial class MarcadorDePosicaoView : UserControl
+    {
+        public MarcadorDePosicaoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
